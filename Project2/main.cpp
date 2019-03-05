7cf04ee03e797ba9bad0b0e4bafb00000ce51b14
Alina Salikhova <alinasalikhova@MacBook-Air-Alina.local>
2019-03-05 17:48:49 +0500

    Initial Commit

---
+++ Project2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project2
+//
+//  Created by Alina Salikhova on 05/03/2019.
+//  Copyright © 2019 Alina Salikhova. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
